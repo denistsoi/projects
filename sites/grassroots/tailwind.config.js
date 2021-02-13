a03bb875a891e7c951cfccd1ac8a7344713b55d4
Denis Tsoi <denistsoi@gmail.com>
2021-02-13 17:05:36 +0800

    ADD: config.tsx for tailwind;

--- sites/grassroots/tailwind.config.js
+++ sites/grassroots/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx', './config.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
