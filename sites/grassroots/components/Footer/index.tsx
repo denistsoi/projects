69b602fb5d8ab19c9dd4bfc6aad89f209b768055
Denis Tsoi <denistsoi@gmail.com>
2021-02-13 20:06:19 +0800

    ADD: update colors;

--- sites/grassroots/components/Footer/index.tsx
+++ sites/grassroots/components/Footer/index.tsx
@@ -1,8 +1,8 @@
 import { Config } from "config"
 
 export const Footer = () => (
-  <footer className="p-4 bg-emerald text-white">
-    <div className="flex flex-wrap items-center justify-between max-w-4xl px-6 mx-auto">
+  <footer className="bg-emerald text-white">
+    <div className="flex flex-wrap items-center justify-between max-w-4xl p-6 mx-auto">
       <p className="text-white flex items-center text-sm">{Config.footer}</p>
     </div>
   </footer>
