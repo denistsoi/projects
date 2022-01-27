927de47fc0bbaeea414d99ef3d047afd7b97540e
wakawakathedev <wakawakathedev@gmail.com>
2022-01-27 22:00:09 +0800

    ADD: use notion to generate media page;

--- sites/grassroots/components/Header/index.tsx
+++ sites/grassroots/components/Header/index.tsx
@@ -2,30 +2,39 @@ import Link from "next/link"
 import Image from "next/image"
 import { Config } from "config"
 
+interface HeaderLink {
+  href: string
+  text: string
+}
+
 export const Header = () => (
   <header className="bg-emerald">
     <div className="flex flex-wrap items-center justify-between max-w-4xl p-6 mx-auto">
-      <Image
-        src="/logo-dark.png"
-        layout="fixed"
-        height={45}
-        width={158}
-        loading="eager"
-        priority
-        alt={Config.alt.logoName}
-      />
+      
+      <Link href="/" >
+        <Image
+          src="/logo-dark.png"
+          layout="fixed"
+          height={45}
+          width={158}
+          loading="eager"
+          priority
+          alt={Config.alt.logoName}
+          className="cursor-pointer"
+        />
+      </Link>
 
-      {/* <div>
-        {Config?.links?.map((link) => {
+      <div className="flex items-between gap-4">
+        {Config?.links?.map((link: HeaderLink) => {
           return (
-            <Link href={link.href}>
-              <span className="text-gray-50 cursor-pointer underline">
+            <Link href={link?.href ?? "#"}>
+              <span className="text-gray-50 cursor-pointer">
                 {link.text}
               </span>
             </Link>
           )
         })}
-      </div> */}
+      </div>
     </div>
   </header>
 )
--- sites/grassroots/config.ts
+++ sites/grassroots/config.ts
@@ -4,9 +4,17 @@ export const Config = {
 
   footer:
     "Grassroots Future is a charitable institution under section 88 of the Inland Revenue Ordinance with No. 91/17131",
+  
   alt: {
     logoName: "Grassroots Future logo",
   },
+
+  links: [
+    {
+      href: "media",
+      text: "Media"
+    },
+  ],
 
   people: [],
 
